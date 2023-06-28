4eb31092d6ab318a5f32c04380ecb2a0c4674275
Tim von Asen <timvonasen@users.noreply.github.com>
2023-06-28 11:11:35 +0200

    TimeSet + README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# NightLight
-Nachtlicht für Softwareentwurf
+# Nachtlicht - Microsoft Azure
+
+## Einführung
+
+Dieses Projekt zeigt, wie man ein intelligentes Nachtlicht mit Microsoft Azure-Diensten verwaltet. Es nutzt das Azure IoT Hub zur Kommunikation zwischen dem Nachtlicht und der Cloud sowie verschiedene weitere Azure-Dienste zur Steuerung und Verwaltung der Lichtfunktionen.
+
+## Inhaltsverzeichnis
+
+- [Einführung](#einführung)
+- [Funktionsübersicht](#funktionsübersicht)
+- [Voraussetzungen](#voraussetzungen)
+- [Lizenz](#lizenz)
+
+## Funktionsübersicht
+
+- **Fernsteuerung des Nachtlichts**: Das Nachtlicht kann über eine Webschnittstelle oder mobile App ein- und ausgeschaltet werden.
+- **Automatische Helligkeitsregelung**: Das System erfasst die Umgebungshelligkeit und passt die Helligkeit des Nachtlichts entsprechend an.
+- **Zeitplanung**: Es können Zeitpläne erstellt werden, nach denen das Nachtlicht automatisch ein- oder ausgeschaltet wird.
+
+## Voraussetzungen
+
+- Microsoft Azure Account
+- Azure IoT Hub
+- Nachtlicht mit IoT-Kompatibilität
+- Internetverbindung
+
+## Lizenz
+
+Dieses Projekt steht unter der MIT-Lizenz. Weitere Informationen finden Sie in der [LICENSE](LICENSE) Datei.
--- script.js
+++ script.js
@@ -1,55 +1,58 @@
-function loadData() {
-  fetch("/sensorData")
-    .then((response) => response.json())
-    .then((data) => {
-      document.getElementById("sensorValue").innerHTML = data.rawValue;
-    });
-}
-
-function updateLedToggle() {
-  fetch("/ledState")
-    .then((response) => response.json())
-    .then((data) => {
-      document.getElementById("ledToggle").checked = data.state;
-    });
-}
-
-function toggleLED() {
-  var ledState = document.getElementById("ledToggle").checked;
-  fetch("/led", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: "state=" + ledState + "&manual=" + true,
-  });
-}
-
-function setTime() {
-  var onTime = document.getElementById("onTime").value;
-  var offTime = document.getElementById("offTime").value;
-  fetch("/timeSet", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: "onTime=" + onTime + "&offTime=" + offTime,
-  });
-}
-
-function updateCurrentTime() {
-  fetch("/currentTime")
-    .then((response) => response.text())
-    .then((time) => {
-      document.getElementById("currentTime").innerHTML = time;
-    });
-}
-
 document.addEventListener("DOMContentLoaded", function () {
-  loadData();
-  updateLedToggle();
   setInterval(function () {
-    loadData();
-    updateCurrentTime();
+    var currentTime = new Date();
+    var hours = currentTime.getHours().toString().padStart(2, "0");
+    var minutes = currentTime.getMinutes().toString().padStart(2, "0");
+    var seconds = currentTime.getSeconds().toString().padStart(2, "0");
+    document.getElementById("currentTime").innerText =
+      hours + ":" + minutes + ":" + seconds;
   }, 1000);
 });
+
+// function loadData() {
+//   fetch("/sensorData")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       document.getElementById("sensorValue").innerHTML = data.rawValue;
+//     });
+// }
+
+// function updateLedToggle() {
+//   fetch("/ledState")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       document.getElementById("ledToggle").checked = data.state;
+//     });
+// }
+
+// function toggleLED() {
+//   var ledState = document.getElementById("ledToggle").checked;
+//   fetch("/led", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/x-www-form-urlencoded",
+//     },
+//     body: "state=" + ledState + "&manual=" + true,
+//   });
+// }
+
+// function setTime() {
+//   var onTime = document.getElementById("onTime").value;
+//   var offTime = document.getElementById("offTime").value;
+//   fetch("/timeSet", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/x-www-form-urlencoded",
+//     },
+//     body: "onTime=" + onTime + "&offTime=" + offTime,
+//   });
+// }
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   loadData();
+//   updateLedToggle();
+//   setInterval(function () {
+//     loadData();
+//     updateCurrentTime();
+//   }, 1000);
+// });
